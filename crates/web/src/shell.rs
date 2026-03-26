134a43e92dd8dc9eb866aa9e006c3fafa23253e6
pianojames <pianoplayerjames@gmail.com>
2026-03-26 09:26:32 +0000

    Add favicon

--- crates/web/src/shell.rs
+++ crates/web/src/shell.rs
@@ -12,6 +12,7 @@ pub fn Shell() -> impl IntoView {
             <head>
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" type="image/x-icon" href="/assets/favicon.ico" />
                 <script src="https://cdn.tailwindcss.com"></script>
                 <link rel="stylesheet" href="https://unpkg.com/@phosphor-icons/web@2.1.1/src/regular/style.css" />
                 <link rel="stylesheet" href="https://unpkg.com/@phosphor-icons/web@2.1.1/src/fill/style.css" />
